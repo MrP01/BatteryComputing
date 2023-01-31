72eacbbb3a78e4e69d414ee2aa6b09702c117988
emiherbrot <hopjedropje@gmail.com>
2023-01-31 17:51:35 +0000

    Python code for chrono

--- Full_Solution_emi.py
+++ Full_Solution_emi.py
@@ -3,6 +3,7 @@
 from scipy.sparse import dia_matrix, dok_matrix
 from scipy.sparse.linalg import spsolve
 import numpy as np
+from scipy.sparse import diags
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
@@ -16,47 +17,62 @@ T0 = 0 ; T = 1
 #dx = 0.1 ; dt = 0.01
 
 # Number of meshpoints and meshsizes
-Nx=10; Nt=10
+Nx=2; Nt=2
 dx=(xn-x0)/Nx ; dt = (T-T0)/Nt
 
 # Courant Friedrichs-Lewy
-CFL = dt/dx^2;
+CFL = dt/dx**2;
 
 # Values at boundary in space and time
 BC1 = 0; BC2 = 1
 IC = 1
 
 # Set up matrices for solving the scheme
-[A] = schemeDirichlet(Nx-1,Nt,CFL);
+A = schemeDirichlet(Nx,Nt,CFL);
+print(A.toarray())
+
+
 #[bs] = rhsDirichlet(Nx-1,Nt, CFL,BC1,BC2,IC);
 #U1 = A\bs;
 #U1 = full(U1);
 #counter = 0;
 
 # Function creating the scheme matrix for a
-def schemeDirichlet(Nx,Nt,m):
+def schemeDirichlet(Nx,Nt,CFL):
 
     #diagonal vector with (Nx+1)*(Nt+1) entries
-    diag=np.zeros((Nx+1)*(Nt+1))+1
+    diag=np.ones((Nx+1)*(Nt+1))*(1+2*CFL)
+
+    # off diagonal vectors
+    udiag = np.ones((Nx + 1) * (Nt + 1) - 1)*(-CFL)
+    ldiag = np.ones((Nx + 1) * (Nt + 1) - 1)*(-CFL)
 
     #lonely diagonal vector accounts for the explicit coefficient
-    lonelydiag=np.zeros((Nx+1)*(Nt+1)-(Nx+1))+1
+    lonelydiag=np.ones((Nx+1)*(Nt+1)-(Nx+1))
 
-    #off diagonal vectors
-    udiag=np.zeros((Nx+1)*(Nt+1)-1)
-    ldiag = np.zeros((Nx + 1) * (Nt + 1) - 1)
 
     # We aim to zero the entries of the lonely vector corresponding to boundary
     # We aim to have tridiagonal matrix (lowerdiag, diag, upperdiag) with (-CFl, 1+2CFL,CFL)
-    for t in range(1,Nt+1):  #so for times t=1,2,...Nt
-        lonelydiag(t*Nx+1)=0
-        lonelydiag((t+1)*Nx-1)=0
-        for k in range(2, Nx): #so for k=2...Nx-1
-            diag(t*Nx+k) = 1+2*CFL
-            udiag(t*Nx+k) = -CFL
-            ldiag(t*Nx+k-1)=-CFL
 
-    A= dia_matrix(([diag, udiag, ldiag, lonelydiag], [0, 1, -1, Nx+1]),shape=(Nx+1, Nt+1))
-return A
+    for t in range(0,Nt+1):
+        diag[t*(Nx+1)]=1
+        diag[t*(Nx+1)+Nx]=1
+        ldiag[t * (Nx + 1) + Nx-1] = 0
+        udiag[t * (Nx + 1)] = 0
 
-print(A.toarray())
+        if t!=Nt:
+            ldiag[t*(Nx+1)+Nx]=0
+            udiag[t * (Nx + 1) + Nx] = 0
+            lonelydiag[t * (Nx + 1)] = 0
+            lonelydiag[t * (Nx + 1) + Nx] = 0
+
+
+    for k in range(0,Nx+1):
+        diag[k]=1
+        ldiag[k]=0
+        udiag[k]=0
+
+
+    A= diags([diag, udiag, ldiag, lonelydiag], [0, 1, -1, -(Nx+1)])
+    return A
+
